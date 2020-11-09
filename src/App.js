9a717d62ed21f260610b7f0a53bd2d08c160faed
naveenkarthika <yuvannaveen65@gmail.com>
2020-11-09 07:58:56 +0000

    ToggleHandler method is added in this commit

--- src/App.js
+++ src/App.js
@@ -4,41 +4,70 @@ import Person from './Person/Person'
 
 class App extends Component {
   state = {
-    persons : [
-      { name : "NaveenKumar" , age : 20},
-      { name : "Vel" , age : 21},
-      { name : "Murugan" , age : 22},
+    persons: [
+      { name: "NaveenKumar", age: 20 },
+      { name: "Vel", age: 21 },
+      { name: "Murugan", age: 22 },
     ],
-    otherState: "Some thing"
+    otherState: "Some thing",
+    showPersons: false
   }
 
-  
 
-   switchNameHandler = (e) => {
-    console.log("clicked")
-    
+
+  switchNameHandler = (event) => {
     this.setState({
-      persons : [
-        { name : "Naveen A" , age : 21},
-        { name : "Velan" , age : 21},
-        { name : "Vel Murugan" , age : 20},
+      persons: [
+        { name: event.target.value, age: 21 },
+        { name: 'Naveen', age: 22 },
+        { name: 'Kumar', age: 20 },
       ]
     })
   }
 
-  render(){
+  ToggleHandler = () => {
+    const doesShow = this.state.showPersons
+    this.setState({
+      showPersons: !doesShow
+    })
+  }
+
+  
+
+  render() {
+    const style = {
+      backgroundColor : 'white',
+      border : '1px solid blue',
+      padding : '10px',
+      font : 'inherit'
+
+    }
     return (
       <div className="App">
         <h2>Hello World!!!!!</h2>
-        <button onClick={this.switchNameHandler}>Toggle Name</button>
-        <Person name={this.state.persons[0].name} age={this.state.persons[0].age}/>
-        <Person name={this.state.persons[1].name} age={this.state.persons[1].age}>My Hobbie is : Cricket</Person>
-        <Person name={this.state.persons[2].name} age={this.state.persons[2].age}/>
+        <button style={style} onClick={this.ToggleHandler}>Toggle Name</button>
+        { this.state.showPersons === true ?
+          <div>
+            <Person
+              name={this.state.persons[0].name}
+              age={this.state.persons[0].age}
+              clicked={this.switchNameHandler} />
+            <Person
+              name={this.state.persons[1].name}
+              age={this.state.persons[1].age}>My Hobbie is : Cricket</Person>
+            <Person
+              name={this.state.persons[2].name}
+              age={this.state.persons[2].age} 
+               />
+          </div>
+          : null
+        }
+
       </div>
     )
   }
-    
-  
+
+
 }
 
 export default App
