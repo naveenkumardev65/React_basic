966d5d9869019bae1025a28830deeb2db528774a
naveenkarthika <yuvannaveen65@gmail.com>
2020-11-09 04:33:46 +0000

    pass the username to userOutput component via props

--- src/App.js
+++ src/App.js
@@ -1,65 +1,18 @@
 import React, { useState } from 'react';
 import './App.css';
 import Person from './Person/Person'
+import UserInput from './UserInput/UserInput'
+import UserOutput from './UserOutput/UserOutput'
 
 const App = (props) => {
-
-  const [personState,setPersonState] = useState({
-    persons : [
-          { name : "NaveenKumar" , age : 20},
-          { name : "Vel" , age : 21},
-          { name : "Murugan" , age : 22},
-        ],
-        otherState: "Some thing"
-  })
-
-  // state = {
-  //   persons : [
-  //     { name : "NaveenKumar" , age : 20},
-  //     { name : "Vel" , age : 21},
-  //     { name : "Murugan" , age : 22},
-  //   ],
-  //   otherState: "Some thing"
-  // }
-
-  const nameChangeHandler = (event) =>{
-    setPersonState({
-      persons : [
-        { name : event.target.value , age : 21},
-        { name : "Vel" , age : 21},
-        { name : "Murugan" , age : 20},
-      ]
-    })
-  }
   
-
-  const switchNameHandler = () => {
-    
-    
-    setPersonState({
-      persons : [
-        { name : "Naveen A" , age : 21},
-        { name : "Vel" , age : 21},
-        { name : "Murugan" , age : 20},
-      ]
-    })
-  }
-
-  const style = {
-    backgroundColor : 'white',
-    padding : '8px',
-    border : '1px solid blue',
-    font : 'inherit',
-    cursor : 'pointer'
-  }
-
+  
     return (
       <div className="App">
-        <h2>Hello World!!!!!</h2>
-        <button onClick={switchNameHandler} style={style}>Switch Name</button>
-        <Person name={personState.persons[0].name} age={personState.persons[0].age} clicked={nameChangeHandler} />
-        <Person name={personState.persons[1].name} age={personState.persons[1].age}>My Hobbie is : Cricket</Person>
-        <Person name={personState.persons[2].name} age={personState.persons[2].age}/>
+        <UserInput />
+        <UserOutput name="Murugan"/>
+        <UserOutput name="Naveen"/>
+        <UserOutput name="Ravin"/>
       </div>
     )
   
