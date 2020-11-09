5f43b6a2309ef9370a9c8f5e1ca4528ecb219a85
naveenkarthika <yuvannaveen65@gmail.com>
2020-11-09 05:11:01 +0000

    Styles added in this commit

--- src/UserOutput/UserOutput.js
+++ src/UserOutput/UserOutput.js
@@ -2,8 +2,16 @@ import React from 'react'
 
 
 const userOutput = (props) => {
+    const style = {
+        width : '50%',
+        marign : '16px auto',
+        border : '1px solid #eee',
+        boxShadow : '0 2px 3px #ccc',
+        padding : '10px',
+        textAlign : 'center'
+    }
     return (
-        <div>
+        <div style={style}>
             <p>I am {props.name}</p>
             <p>I am Web developer</p>
         </div>
