d75854f654dfb5e01405524a22fdfb38b54ee0cb
naveenkarthika <yuvannaveen65@gmail.com>
2020-11-09 04:34:09 +0000

    pass the username to userOutput component via props

---
+++ src/UserInput/UserInput.js
@@ -0,0 +1,10 @@
+import React from 'react'
+
+
+const userInput = () => {
+    return (
+        <input type="text" />
+    )
+}
+
+export default userInput
---
+++ src/UserOutput/UserOutput.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+
+const userOutput = (props) => {
+    return (
+        <div>
+            <p>I am {props.name}</p>
+            <p>I am Web developer</p>
+        </div>
+
+    )
+}
+
+export default userOutput
