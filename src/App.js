701b94a97c7195c72a85a891c52f5f4951768511
Mohan <uvimohan54@gmail.com>
2020-11-18 13:55:24 +0530

    In this commit add the namchangedHandler method to chenge the indiviual input field

--- src/App.js
+++ src/App.js
@@ -5,9 +5,9 @@ import Person from './Person/Person'
 class App extends Component {
   state = {
     persons: [
-      { name: "NaveenKumar", age: 20 },
-      { name: "Vel", age: 21 },
-      { name: "Murugan", age: 22 },
+      { id:'aadsd',name: "NaveenKumar", age: 20 },
+      { id:'wrqss',name: "Vel", age: 21 },
+      { id:'bbdfd',name: "Murugan", age: 22 },
     ],
     otherState: "Some thing",
     showPersons: false
@@ -15,14 +15,18 @@ class App extends Component {
 
 
 
-  switchNameHandler = (event) => {
-    this.setState({
-      persons: [
-        { name: event.target.value, age: 21 },
-        { name: 'Naveen', age: 22 },
-        { name: 'Kumar', age: 20 },
-      ]
+  nameChangedHandler = (event,id) => {
+    const personIndex = this.state.persons.findIndex(p => {
+      return p.id === id;
     })
+    const person = {
+      ...this.state.persons[personIndex]
+    };
+    person.name = event.target.value;
+    const persons = [...this.state.persons];
+    persons[personIndex]=person;
+   
+    this.setState({  persons : persons  });
   }
 
   togglePersonHandler = () => {
@@ -54,9 +58,10 @@ class App extends Component {
             this.state.persons.map((person,index)=>{
               return <Person
               click={() => this.deletePersonHandler(index)}
-              key={index} 
+              key={person.id} 
               name={person.name}
               age={person.age}
+              changed={(event) => this.nameChangedHandler(event,person.id)}
               />
             })
           }
--- src/Person/Person.js
+++ src/Person/Person.js
@@ -7,7 +7,7 @@ const person = (props) => {
             {/* <p>I am Person,my age is {Math.floor(Math.random() *30)}</p> */}
             <p onClick={props.click}>My name is {props.name},my age is {props.age}</p>
             <p>{props.children}</p>
-            <input type="text" onChange={props.clicked} value={props.name}/>
+            <input type="text" onChange={props.changed} value={props.name} />
         </div>
     );
 }
