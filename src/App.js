3448c8282f4b4c38fb5bab71b0d54fa645c1017b
naveenkarthika <yuvannaveen65@gmail.com>
2020-11-24 20:40:38 +0530

    This commit style-component module is installed to rearrage the style redium into normal styles

--- src/App.js
+++ src/App.js
@@ -1,16 +1,27 @@
-import { CallMerge } from '@material-ui/icons';
 import React, { Component } from 'react';
 import './App.css';
 import Person from './Person/Person'
-import Redium from 'radium'
-import { lightGreen } from '@material-ui/core/colors';
+import styled from 'styled-components'
+
+const StyleButton = styled.button`
+  background-color: green;
+  color: white;
+  border: 1px solid blue;
+  padding: 10px;
+  font: inherit;
+
+  &:hover {
+    background-color:lightgreen;
+    color:black;
+  }
+`;
 
 class App extends Component {
   state = {
     persons: [
-      { id:'aadsd',name: "NaveenKumar", age: 20 },
-      { id:'wrqss',name: "Vel", age: 21 },
-      { id:'bbdfd',name: "Murugan", age: 22 },
+      { id: 'aadsd', name: "NaveenKumar", age: 20 },
+      { id: 'wrqss', name: "Vel", age: 21 },
+      { id: 'bbdfd', name: "Murugan", age: 22 },
     ],
     otherState: "Some thing",
     showPersons: false
@@ -18,7 +29,7 @@ class App extends Component {
 
 
 
-  nameChangedHandler = (event,id) => {
+  nameChangedHandler = (event, id) => {
     const personIndex = this.state.persons.findIndex(p => {
       return p.id === id;
     })
@@ -27,9 +38,9 @@ class App extends Component {
     };
     person.name = event.target.value;
     const persons = [...this.state.persons];
-    persons[personIndex]=person;
-   
-    this.setState({  persons : persons  });
+    persons[personIndex] = person;
+
+    this.setState({ persons: persons });
   }
 
   togglePersonHandler = () => {
@@ -42,68 +53,60 @@ class App extends Component {
 
   deletePersonHandler = (personIndex) => {
     const persons = [...this.state.persons];
-    persons.splice(personIndex,1)
-    this.setState({ persons : persons})
+    persons.splice(personIndex, 1)
+    this.setState({ persons: persons })
   }
 
   render() {
-    const style = {
-      backgroundColor: 'green',
-      color:'white',
-      border: '1px solid blue',
-      padding: '10px',
-      font: 'inherit',
-      ':hover':{
-        backgroundColor:'lightGreen',
-        color:'black'
-      }
-    }
-
+   
     let persons = null
     if (this.state.showPersons) {
       persons = (
         <div>
           {
-            this.state.persons.map((person,index)=>{
+            this.state.persons.map((person, index) => {
               return <Person
-              click={() => this.deletePersonHandler(index)}
-              key={person.id} 
-              name={person.name}
-              age={person.age}
-              changed={(event) => this.nameChangedHandler(event,person.id)}
+                click={() => this.deletePersonHandler(index)}
+                key={person.id}
+                name={person.name}
+                age={person.age}
+                changed={(event) => this.nameChangedHandler(event, person.id)}
               />
             })
           }
         </div>
       )
-      style.backgroundColor = "red"
-      style[':hover']= {
-        backgroundColor:'salmon',
-        color:'black'
-      }
+      StyleButton.backgroundColor = "red"
+      // style[':hover'] = {
+      //   backgroundColor: 'salmon',
+      //   color: 'black'
+      // }
 
     }
-    let classes=[];
-    
-    if(this.state.persons.length <=2){
+    let classes = [];
+
+    if (this.state.persons.length <= 2) {
       classes.push('red');
     }
-    if(this.state.persons.length <=1){
+    if (this.state.persons.length <= 1) {
       classes.push('bold');
     }
-    
+
     return (
-      <div className="App">
-        <h2>Hello World!!!!!</h2>
-        <p className={classes.join(' ')}>It working now!!!!</p>
-        <button style={style} onClick={this.togglePersonHandler}>Toggle Name</button>
-        {persons}
-      </div>
+      
+        <div className="App">
+          <h2>Hello World!!!!!</h2>
+          <p className={classes.join(' ')}>It working now!!!!</p>
+          <StyleButton onClick={this.togglePersonHandler}>Toggle Name</StyleButton>
+          {persons}
+        </div>
+    
+
     )
   }
 
 
 }
 
-export default Redium(App)
+export default App
 
--- src/Person/Person.js
+++ src/Person/Person.js
@@ -1,14 +1,33 @@
 import React from 'react'
-import '../Person/Person.css'
+// import '../Person/Person.css'
+import styled from 'styled-components'
+
+const StyleDiv = styled.div`
+    width:60%;
+    border:1px solid #eee;
+    box-shadow:0 2px 3px #ccc;
+    padding:10px;
+    margin:16px auto;
+    text-align:center;
+
+    @media (min-width : 500px){
+        width: 460px;
+    }
+`;
 
 const person = (props) => {
+
     return (
-        <div className="Person">
-            {/* <p>I am Person,my age is {Math.floor(Math.random() *30)}</p> */}
-            <p onClick={props.click}>My name is {props.name},my age is {props.age}</p>
-            <p>{props.children}</p>
-            <input type="text" onChange={props.changed} value={props.name} />
-        </div>
+        // <div className="Person" style={style}>
+            <StyleDiv>
+                {/* <p>I am Person,my age is {Math.floor(Math.random() *30)}</p> */}
+                <p onClick={props.click}>My name is {props.name},my age is {props.age}</p>
+                <p>{props.children}</p>
+                <input type="text" onChange={props.changed} value={props.name} />
+            </StyleDiv>
+
+        // </div>
+
     );
 }
 export default person
